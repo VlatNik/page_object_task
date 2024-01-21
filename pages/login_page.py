7062612f4865a09eaa9875c3171edbe71cb0f920
Николаев Владислав Андреевич <Vladislav.Nikolaev@norbit.ru>
2024-01-21 18:35:48 +0300

    fix bugs

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,16 +2,16 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+	def should_be_login_page(self):
+		self.should_be_login_url()
+		self.should_be_login_form()
+		self.should_be_register_form()
+		
+	def should_be_login_url(self):
+		assert "login" in self.browser.current_url, "Login is not presented in url"
 
-    def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "Login is not presented in url"
-
-    def should_be_login_form(self):
+	def should_be_login_form(self):
 		assert self.is_element_present(*LoginPageLocators.Login_Form), "Login form is not presented"
 
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.Reg_Form), "Register form is not presented"
+	def should_be_register_form(self):
+		assert self.is_element_present(*LoginPageLocators.Reg_Form), "Register form is not presented"
